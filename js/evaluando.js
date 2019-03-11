6f245ca7d17655c69f3379b72e124c4f633ed0b7
Jasorza <41799764+Jasorza@users.noreply.github.com>
2019-03-11 17:52:22 -0500

    Initial commit

---
+++ js/evaluando.js
@@ -0,0 +1,34 @@
+'user restrick'
+//Funcion de autoinvocacion y  anonima para cuando queremos usar la funcion solo para una cosa
+// ( function () {
+//      var x="hello";
+// } );
+//Funcion clasica puede tener mas de un parametro o ninguno
+// function llamar() {
+//     console.log('Hello');
+// };
+// //Funcion flecha o landa
+// const x=(x,y) =>x*y;
+// //Funcion de retorno con parametros
+// function sumar(num1,num2) {
+//     var datos=num1+num2;
+//     console.log('Hello you result is: '+datos);
+// } sumar(5,6);
+// //Funcion de retorno sin parametros
+// function sumar() {
+//     var dato=5+5;
+//     return dato;
+// }sumar();
+// // Con var se definen variables globales, para que en dado caso que el programdor quiera cambiar y usar esta con mas libertad pueda hacerlo
+// //Con let se definen variables locales por ende nos da restricciones a la hora de usarla esta en especial se usa para diferenciar que dato sumnistra o elige el usuario en dado caso una encuesta de musica 
+//  var elige='rock';
+
+//  let eleccion='jazz';
+
+//  let =document.querySelector('.caja');
+//  console.table(1,2,[elige,eleccion]);
+//  const nombre=(dato1,dat2,dato3) => (dato1+dato3+dat2);
+//  console.log(nombre('Carlos','Andres','Mariño'));  
+
+//SE recorren usando los metodos for, foreach,forof
+//querySelectorAll
